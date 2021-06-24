9f22c8e5700413ea865f28d762f797989990e6a6
Ola Ajigboye <dipo@ebi.ac.uk>
2021-06-24 11:31:14 +0100

    Add HomeController

---
+++ semanticmodel/src/main/java/org/ejprd/semanticmodel/HomeController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.ejprd.semanticmodel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Siva
+ *
+ */
+@Controller
+public class HomeController
+{
+	@RequestMapping("/semanticmodel")
+	public String home(Model model)
+	{
+			return "index";
+	}
+}
