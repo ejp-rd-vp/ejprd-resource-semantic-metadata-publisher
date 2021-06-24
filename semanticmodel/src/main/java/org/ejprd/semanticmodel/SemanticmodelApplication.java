c39d51b8a31150533ee5f9c72825a75f59bd0e59
Ola Ajigboye <dipo@ebi.ac.uk>
2021-06-24 11:26:51 +0100

    New modules

---
+++ semanticmodel/src/main/java/org/ejprd/semanticmodel/SemanticmodelApplication.java
@@ -0,0 +1,13 @@
+package org.ejprd.semanticmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticmodelApplication.class, args);
+	}
+
+}
---
+++ semanticmodel/src/test/java/org/ejprd/semanticmodel/SemanticmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ejprd.semanticmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanticmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
